eb76734bf601caa3f829a37e10357d68c3b1b9f1
luisaborgeszup <luisa.borges@zup.com.br>
2019-08-19 16:02:23 -0300

    Atualização de estilo para a função search da aplicação

--- Curso-ReactJs-Ninja/Módulo 1/Seção 2/Github App/src/components/search.js
+++ Curso-ReactJs-Ninja/Módulo 1/Seção 2/Github App/src/components/search.js
@@ -1,19 +1,29 @@
 'use strict'
 
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 const Search = ({isDisabled, handleSearch}) => (
-  <div className='search'>
-    <input
-      className='input'
-      type='search'
-      placeholder='Digite o nome do usuário'
-      disabled={isDisabled}
-      onKeyUp={handleSearch}
-    />
-    <p />
-  </div>
+  <Fragment>
+    <div className='search' className='search-box' id='searchField'>
+      <p>Digite o nome do usuário</p>
+      <input
+        type='search'
+        placeholder='...'
+        disabled={isDisabled}
+        onKeyUp={handleSearch}
+      />
+    </div>
+    <div className='search' className='search-box-two' id='searchFieldAfterResult'>
+      <p>Digite o nome do usuário</p>
+      <input
+        type='search'
+        placeholder='...'
+        disabled={isDisabled}
+        onKeyUp={handleSearch}
+      />
+    </div>
+  </Fragment>
 )
 
 Search.propTypes = {
